b259c668e35f055a947d63d30a483339ef936ab5
Antonio-pagliacci <contato.antoniopagliacci@gmail.com>
2025-08-28 13:14:49 -0300

    correção rota login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
     },
 
     {
-        path: 'login-page',
+        path: 'login',
         loadComponent: () => import('./pages/login-page/login-page.component').then(c => c.LoginPageComponent)
     }
 
